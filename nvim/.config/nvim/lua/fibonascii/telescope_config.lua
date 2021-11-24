5946fd223b658c47713dbae37267931088ee0286
Reagan James <reaganjkirby@gmail.com>
2021-11-24 14:24:05 -0600

    Fixed file ignore pattern to remove plugged dir from dotfiles search

--- nvim/.config/nvim/lua/fibonascii/telescope_config.lua
+++ nvim/.config/nvim/lua/fibonascii/telescope_config.lua
@@ -15,7 +15,7 @@ require("telescope").setup({
         grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
         qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
         file_ignore_patterns = {
-         "./plugged/*"
+         "^plugged/"
     },
 
         mappings = {
